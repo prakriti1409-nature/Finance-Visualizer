8a900776475a651d8f9b2b74e8cbc8938d57c180
Prakriti Yadav <prakritiyadav2004@gmail.com>
2025-07-06 19:17:08 +0530

    solved2

--- src/app/api/budgets/route.ts
+++ src/app/api/budgets/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectToDatabase from '@/lib/mongodb';
 import { Budget } from '@/models/Budget';
 
-export async function GET(_: NextRequest) {
+export async function GET() {
   await connectToDatabase();
   const budgets = await Budget.find().lean();
   return NextResponse.json(budgets);
